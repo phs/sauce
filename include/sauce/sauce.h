fddc2c4513d0ddb786df6eca869b6c032e7f348a
Phil Smith <phil.h.smith@gmail.com>
2011-04-12 22:42:15 -0700

    Start up a header and test.

---
+++ include/sauce/sauce.h
@@ -0,0 +1,7 @@
+#ifndef SAUCE_SAUCE_H_
+#define SAUCE_SAUCE_H_
+
+namespace sauce {
+}
+
+#endif
---
+++ test/sauce_test.h
@@ -0,0 +1,16 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <sauce/sauce.h>
+
+namespace sauce {
+
+class SauceTest : public ::testing::Test {
+public:
+
+};
+
+TEST_F(SauceTest, should_derp) {
+}
+
+} // namespace sauce
