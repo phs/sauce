4341460a0fae865564e1ee3eabff231a335b203e
Phil Smith <phil.h.smith@gmail.com>
2011-05-01 12:02:00 -0700

    Extract binding types to generated header.
    Don't bother trying to deal with arity loops in the template yet.

--- include/sauce/sauce.h
+++ include/sauce/sauce.h
@@ -1,6 +1,8 @@
 #ifndef SAUCE_SAUCE_H_
 #define SAUCE_SAUCE_H_
 
+#include <sauce/internal/generated_bindings.h>
+
 namespace sauce {
 
   template<typename Module>
@@ -19,38 +21,6 @@ namespace sauce {
 
   };
 
-  namespace internal {
-
-    template<typename Injector, typename F>
-    struct NewBinding;
-
-    template<typename Injector, typename Dependency>
-    struct NewBinding<Injector, Dependency()> {
-      static Dependency & provide(Injector & injector) {
-        return *new Dependency();
-      };
-    };
-
-    template<typename Injector, typename Dependency, typename A1>
-    struct NewBinding<Injector, Dependency(A1)> {
-      static Dependency & provide(Injector & injector) {
-        return *new Dependency(
-          injector.template provide<A1>()
-        );
-      };
-    };
-
-    template<typename Injector, typename Dependency, typename A1, typename A2>
-    struct NewBinding<Injector, Dependency(A1, A2)> {
-      static Dependency & provide(Injector & injector) {
-        return *new Dependency(
-          injector.template provide<A1>(),
-          injector.template provide<A2>()
-        );
-      };
-    };
-
-  } // namespace internal
 } // namespace sauce
 
 #endif
