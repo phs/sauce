969e4ad35e5fe19f3da0ff1c73ae5fcab43a62c3
Phil Smith <phil.h.smith@gmail.com>
2011-05-24 22:47:43 -0700

    Extract the injector into injector.h

---
+++ include/sauce/injector.h
@@ -0,0 +1,58 @@
+#ifndef SAUCE_SAUCE_INJECTOR_H_
+#define SAUCE_SAUCE_INJECTOR_H_
+
+#include <sauce/internal/bindings.h>
+#include <sauce/internal/new_delete.h>
+
+namespace sauce {
+
+  namespace test {
+    class SauceTest;
+  }
+
+  template<typename Module, typename NewDelete = ::sauce::internal::NewDelete>
+  class Injector {
+  private:
+
+    typedef NewDelete _NewDelete;
+    typedef Injector<Module, NewDelete> _Injector;
+
+  public:
+
+    friend class ::sauce::test::SauceTest;
+    friend class ::sauce::internal::bindings::Binding<_Injector>;
+
+    Injector():
+      new_delete() {}
+
+    virtual ~Injector() {}
+
+    template<typename Iface>
+    Iface provide() {
+      return provide<Iface>(Module::template bindings<_Injector>);
+    }
+
+    template<typename Iface>
+    void dispose(Iface iface) {
+      dispose<Iface>(Module::template bindings<_Injector>, iface);
+    }
+
+  private:
+
+    NewDelete new_delete;
+
+    template<typename Iface, typename Binding>
+    Iface provide(Binding *binding (Iface)) {
+      return Binding::provide(*this);
+    }
+
+    template<typename Iface, typename Binding>
+    void dispose(Binding *binding (Iface), Iface iface) {
+      Binding::dispose(*this, iface);
+    }
+
+  };
+
+} // namespace sauce
+
+#endif
