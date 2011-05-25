2a07c2f76b8ac8928bddcb6d0b26eed78451b18d
Phil Smith <phil.h.smith@gmail.com>
2011-05-24 22:33:08 -0700

    Forgot to add the new include.

---
+++ include/sauce/exceptions.h
@@ -0,0 +1,16 @@
+#ifndef SAUCE_SAUCE_EXCEPTIONS_H_
+#define SAUCE_SAUCE_EXCEPTIONS_H_
+
+namespace sauce {
+
+  /*
+    Thrown when disposing a value that was not provided by the injector.
+  */
+  class UnknownDisposal: public std::runtime_error {
+  public:
+    UnknownDisposal(): std::runtime_error("UnknownDisposal") {}
+  };
+
+} // namespace sauce
+
+#endif
