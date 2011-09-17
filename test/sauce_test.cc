cd80bc2c6a89a9c0e852c4e2e3ea112b27895afd
Phil Smith <phil.h.smith@gmail.com>
2011-09-17 13:33:44 -0700

    Prove transitive provisions and disposals work.
    Disposals are not performed in reverse-topological-sort order from provisions, but rather in the same topological order.  This is because the smart pointer cleanup happens after the destructor, which is where dependencies are disposed.

--- test/sauce_test.cc
+++ test/sauce_test.cc
@@ -21,10 +21,14 @@ class Vehicle {};
 class Herbie:
   public Vehicle {
 public:
-  Chasis & chasis;
-  Engine & engine;
+  SAUCE_SHARED_PTR<Chasis> chasis;
+  SAUCE_SHARED_PTR<Engine> engine;
 
-  Herbie(Chasis & chasis, Engine & engine):
+  Herbie():
+    chasis(),
+    engine() {}
+
+  Herbie(SAUCE_SHARED_PTR<Chasis> chasis, SAUCE_SHARED_PTR<Engine> engine):
     chasis(chasis),
     engine(engine) {}
 };
@@ -43,7 +47,7 @@ public:
   }
 
   template<typename Injector>
-  static ::sauce::internal::bindings::New<Injector, Vehicle, Herbie(Chasis &, Engine &)> * bindings(
+  static ::sauce::internal::bindings::New<Injector, Vehicle, Herbie(Chasis, Engine)> * bindings(
     Vehicle *) {
     return 0;
   }
@@ -63,12 +67,13 @@ public:
 
   template<class C, typename A1, typename A2>
   C * new_(A1, A2);
+
   template<class C>
   void delete_(C *);
 
   MOCK_METHOD0(newCoupChasis, CoupChasis * ());
   MOCK_METHOD0(newHybridEngine, HybridEngine * ());
-  MOCK_METHOD2(newHerbie, Herbie * (Chasis *, Engine *));
+  MOCK_METHOD2(newHerbie, Herbie * (SAUCE_SHARED_PTR<Chasis>, SAUCE_SHARED_PTR<Engine> ));
 
   MOCK_METHOD1(deleteChasis, void(Chasis *));
   MOCK_METHOD1(deleteEngine, void(Engine *));
@@ -87,11 +92,9 @@ HybridEngine * MockNewDelete::new_<HybridEngine>() {
 }
 
 template<>
-Herbie * MockNewDelete::new_<Herbie>(Chasis & chasis, Engine & engine) {
-  // Use addresses here only because googletest doesn't know how to
-  // deal with references aside from copying them (and thus breaking my
-  // object-identity comparators)
-  return newHerbie(&chasis, &engine);
+Herbie * MockNewDelete::new_<Herbie>(SAUCE_SHARED_PTR<Chasis> chasis,
+                                     SAUCE_SHARED_PTR<Engine> engine) {
+  return newHerbie(chasis, engine);
 }
 
 template<>
@@ -132,40 +135,44 @@ TEST_F(SauceTest, shouldProvideAndDisposeADependency) {
   ASSERT_EQ(&chasis, actual.get());
 }
 
-// TEST_F(SauceTest, shouldProvideAndDisposeOfDependenciesTransitively) {
-//   CoupChasis chasis;
-//   HybridEngine engine;
-//   Herbie vehicle(chasis, engine);
-//
-//   // We don't care about the relative ordering between chasis and engine:
-//   // only about how they stand relative to the vehicle.
-//   Sequence injectedChasis, injectedEngine;
-//
-//   EXPECT_CALL(newDelete, newCoupChasis()).
-//   InSequence(injectedChasis).
-//   WillOnce(Return(&chasis));
-//
-//   EXPECT_CALL(newDelete, newHybridEngine()).
-//   InSequence(injectedEngine).
-//   WillOnce(Return(&engine));
-//
-//   EXPECT_CALL(newDelete, newHerbie(&chasis, &engine)).
-//   InSequence(injectedChasis, injectedEngine).
-//   WillOnce(Return(&vehicle));
-//
-//   EXPECT_CALL(newDelete, deleteVehicle(&vehicle)).
-//   InSequence(injectedChasis, injectedEngine);
-//
-//   // EXPECT_CALL(newDelete, deleteEngine(&engine)).
-//   // InSequence(injectedEngine);
-//
-//   // EXPECT_CALL(newDelete, deleteChasis(&chasis)).
-//   // InSequence(injectedChasis);
-//
-//   Vehicle * actual = injector.provide<Vehicle *>();
-//   ASSERT_EQ(&vehicle, actual);
-//   injector.dispose<Vehicle *>(actual);
-// }
+// Argument matcher for smart pointers based on backing address.
+MATCHER_P(SmartPointerTo, value, "") {
+  return arg.get() == value;
+}
+
+TEST_F(SauceTest, shouldProvideAndDisposeOfDependenciesTransitively) {
+  CoupChasis chasis;
+  HybridEngine engine;
+  Herbie vehicle;
+
+  // We don't care about the relative ordering between chasis and engine:
+  // only about how they stand relative to the vehicle.
+  Sequence injectedChasis, injectedEngine;
+
+  EXPECT_CALL(newDelete, newCoupChasis()).
+  InSequence(injectedChasis).
+  WillOnce(Return(&chasis));
+
+  EXPECT_CALL(newDelete, newHybridEngine()).
+  InSequence(injectedEngine).
+  WillOnce(Return(&engine));
+
+  EXPECT_CALL(newDelete, newHerbie(SmartPointerTo(&chasis), SmartPointerTo(&engine))).
+  InSequence(injectedChasis, injectedEngine).
+  WillOnce(Return(&vehicle));
+
+  EXPECT_CALL(newDelete, deleteEngine(&engine)).
+  InSequence(injectedEngine);
+
+  EXPECT_CALL(newDelete, deleteChasis(&chasis)).
+  InSequence(injectedChasis);
+
+  EXPECT_CALL(newDelete, deleteVehicle(&vehicle)).
+  InSequence(injectedChasis, injectedEngine);
+
+  SAUCE_SHARED_PTR<Vehicle> actual = injector.provide<Vehicle>();
+  ASSERT_EQ(&vehicle, actual.get());
+}
 
 }
 }
