bd43565576a0d9e842d389c54440055b5e292623
Phil Smith <phil.h.smith@gmail.com>
2011-09-20 19:41:42 -0500

    Revert "doco"
    This reverts commit 77ed743dfcd0dc5dd58b48ddbe32e37267ac151f.
    
    Backing out of scopes a moment.  I think I just found a much better way to specify bindings.

--- include/sauce/module.h
+++ include/sauce/module.h
@@ -7,15 +7,6 @@
 
 namespace sauce {
 
-/**
- * The default set of scopes that bindings may choose from.
- *
- * To define your own set of scopes, declare a Scopes enum in your module.
- *
- * Note that most applications should not need this degree of customization:
- * the granularities suggested by the singleton, session and request scopes
- * are usually applicable, even in contexts other than the web.
- */
 enum DefaultScopes {
   SINGLETON_SCOPE,
   SESSION_SCOPE,
