7a61dc2985b7095d1bc413d014ca3c06e5bc4477
Phil Smith <phil.h.smith@gmail.com>
2011-10-04 23:15:31 -0700

    Add a circular dependency exception.
    Also break out an exception base type.

--- include/sauce/exceptions.h
+++ include/sauce/exceptions.h
@@ -1,19 +1,37 @@
 #ifndef SAUCE_SAUCE_EXCEPTIONS_H_
 #define SAUCE_SAUCE_EXCEPTIONS_H_
 
+#include <string>
 #include <stdexcept>
 
 namespace sauce {
+
+/**
+ * Base class for all sauce exceptions.
+ */
+struct Exception: std::runtime_error {
+  Exception(std::string message):
+    std::runtime_error(message) {}
+};
 
 /**
  * Raised when no binding can be found for a given interface.
  *
  * TODO sure would be nice to know who..
  */
-struct UnboundException:
-  public std::runtime_error {
+struct UnboundException: Exception {
   UnboundException():
-    std::runtime_error("Request for unbound interface.") {}
+    Exception("Request for unbound interface.") {}
+};
+
+/**
+ * Raised when a cycle is found in the interface's dependencies.
+ *
+ * TODO sure would be nice to know what the cycle is..
+ */
+struct CircularDependencyException: Exception {
+  CircularDependencyException():
+    Exception("Request for unbound interface.") {}
 };
 
 }
