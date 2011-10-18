467f5fda3538e10c870949d6aaaf4912eb4f9877
Phil Smith <phil.h.smith@gmail.com>
2011-10-17 23:25:05 -0700

    Add SAUCE_WEAK_PTR macro.

--- include/sauce/memory.h
+++ include/sauce/memory.h
@@ -4,14 +4,17 @@
 #if SAUCE_STD_SMART_PTR
   #include <memory>
   #define SAUCE_SHARED_PTR std::shared_ptr
+  #define SAUCE_WEAK_PTR std::weak_ptr
   #define SAUCE_STATIC_POINTER_CAST std::static_pointer_cast
 #elif SAUCE_STD_TR1_SMART_PTR
   #include <tr1/memory>
   #define SAUCE_SHARED_PTR std::tr1::shared_ptr
+  #define SAUCE_WEAK_PTR std::tr1::weak_ptr
   #define SAUCE_STATIC_POINTER_CAST std::tr1::static_pointer_cast
 #elif SAUCE_BOOST_SMART_PTR
   #include <boost/shared_ptr.hpp>
   #define SAUCE_SHARED_PTR boost::shared_ptr
+  #define SAUCE_WEAK_PTR boost::weak_ptr
   #define SAUCE_STATIC_POINTER_CAST boost::static_pointer_cast
 #else
   #error Please define one of SAUCE_STD_SMART_PTR, SAUCE_STD_TR1_SMART_PTR or SAUCE_BOOST_SMART_PTR
