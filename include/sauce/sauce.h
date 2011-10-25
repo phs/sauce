b733d6a17c0cb89e2f3c42d02aa5146c116ccc0b
Phil Smith <phil.h.smith@gmail.com>
2011-10-25 11:27:38 -0700

    Have a brief main page.

--- include/sauce/sauce.h
+++ include/sauce/sauce.h
@@ -7,4 +7,12 @@
 #include <sauce/named.h>
 #include <sauce/scopes.h>
 
+/**
+ * @mainpage API Reference
+ *
+ * <div style="width: 100%; text-align: center; padding-top: 100px;">
+ * <h1>Please see the <a href="https://github.com/phs/sauce/blob/master/README.md">README</a> for a general introduction.</h1>
+ * </div>
+ */
+
 #endif // SAUCE_SAUCE_H_
