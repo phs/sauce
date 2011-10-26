76d9eb87f33326de52726b2490bb0763de739f75
Phil Smith <phil.h.smith@gmail.com>
2011-10-25 22:49:26 -0700

    Rename new.h -> new_binding.h

--- include/sauce/internal/bindings/all.h
+++ include/sauce/internal/bindings/all.h
@@ -2,6 +2,6 @@
 #define SAUCE_SAUCE_INTERNAL_BINDINGS_ALL_H_
 
 #include <sauce/internal/bindings/transparent.h>
-#include <sauce/internal/bindings/new.h>
+#include <sauce/internal/bindings/new_binding.h>
 
 #endif // SAUCE_SAUCE_INTERNAL_BINDINGS_ALL_H_
