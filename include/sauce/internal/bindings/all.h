0d7257c2689a2f971a446b0a535235b0cd5d8ed7
Phil Smith <phil.h.smith@gmail.com>
2011-10-26 00:11:47 -0700

    Add an include.

--- include/sauce/internal/bindings/all.h
+++ include/sauce/internal/bindings/all.h
@@ -2,6 +2,7 @@
 #define SAUCE_SAUCE_INTERNAL_BINDINGS_ALL_H_
 
 #include <sauce/internal/bindings/transparent_binding.h>
+#include <sauce/internal/bindings/naked_binding.h>
 #include <sauce/internal/bindings/new_binding.h>
 
 #endif // SAUCE_SAUCE_INTERNAL_BINDINGS_ALL_H_
