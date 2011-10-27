bcf673e682d865dd6a71d3d6123cbf0c5a98be84
Phil Smith <phil.h.smith@gmail.com>
2011-10-27 11:54:42 -0700

    Ditch some unneeded includes.

--- include/sauce/internal/bindings.h
+++ include/sauce/internal/bindings.h
@@ -2,7 +2,6 @@
 #define SAUCE_SAUCE_INTERNAL_BINDINGS_H_
 
 #include <map>
-#include <set>
 #include <vector>
 #include <utility>
 
@@ -10,7 +9,6 @@
 
 #include <sauce/exceptions.h>
 #include <sauce/memory.h>
-#include <sauce/named.h>
 #include <sauce/internal/binding.h>
 #include <sauce/internal/key.h>
 #include <sauce/internal/type_id.h>
