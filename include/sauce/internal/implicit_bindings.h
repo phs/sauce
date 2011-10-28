86794f0748e3873fce5288598ed263532e57865c
Phil Smith <phil.h.smith@gmail.com>
2011-10-27 17:53:10 -0700

    The new file.

---
+++ include/sauce/internal/implicit_bindings.h
@@ -0,0 +1,20 @@
+#ifndef SAUCE_SAUCE_INTERNAL_IMPLICIT_BINDINGS_H_
+#define SAUCE_SAUCE_INTERNAL_IMPLICIT_BINDINGS_H_
+
+// #include <sauce/internal/bindings/all.h>
+
+namespace sauce {
+namespace internal {
+
+/**
+ * Attempts to supply a Binding to Bindings when none is found for a dependency.
+ */
+struct ImplicitBindings {};
+
+}
+
+namespace i = ::sauce::internal;
+
+}
+
+#endif // SAUCE_SAUCE_INTERNAL_IMPLICIT_BINDINGS_H_
