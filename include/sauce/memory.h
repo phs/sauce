aa8d84e3a5231160f2a47102a93643551e8606e0
Phil Smith <phil.h.smith@gmail.com>
2012-04-15 20:46:13 -0700

    Trim up a message.

--- include/sauce/memory.h
+++ include/sauce/memory.h
@@ -8,7 +8,7 @@
 #elif SAUCE_BOOST_SMART_PTR
   #include <sauce/internal/memory/boost.h>
 #else
-  #error Please define one of SAUCE_STD_SMART_PTR, SAUCE_STD_TR1_SMART_PTR or SAUCE_BOOST_SMART_PTR
+  #error Please define SAUCE_STD_SMART_PTR, SAUCE_STD_TR1_SMART_PTR or SAUCE_BOOST_SMART_PTR
 #endif
 
 #endif // SAUCE_MEMORY_H_
