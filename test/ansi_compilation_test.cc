7b9da69256d6ddc654027d999cf8706408ec1b83
Phil Smith <phil.h.smith@gmail.com>
2012-06-18 17:08:11 -0700

    Give the ansi checker something to do.

--- test/ansi_compilation_test.cc
+++ test/ansi_compilation_test.cc
@@ -8,7 +8,59 @@
  * It does so by first pulling in all desired includes, instantiating templates and then asserting
  * that it was compiled with -ansi.
  */
+
+using ::sauce::AbstractProvider;
+using ::sauce::Binder;
+using ::sauce::Injector;
+using ::sauce::Modules;
+using ::sauce::Named;
+
+struct Animal {
+  virtual std::string says() = 0;
+};
+
+struct Cat: Animal {
+  std::string says() { return "Meow"; }
+};
+
+struct LieutenantShinysides {};
+struct Fish: Animal {
+  std::string says() { return "Blub blub"; }
+};
+
+struct Meatloaf {};
+struct Cow: Animal {
+  std::string says() { return "Moo"; }
+};
+
+struct CowProvider: AbstractProvider<Animal> {
+  Animal * provide() {
+    return new Cow();
+  }
+
+  void dispose(Animal * cow) {
+    delete cow;
+  }
+};
+
+struct Pond {
+  sauce::shared_ptr<Animal> animal;
+
+  Pond(sauce::shared_ptr<Animal> animal):
+    animal(animal) {}
+};
+
+void AnimalModule(Binder & binder) {
+  binder.bind<Animal>().to<Cat()>();
+  binder.bind<Animal>().named<LieutenantShinysides>().to<Fish()>();
+  binder.bind<Animal>().named<Meatloaf>().toProvider<CowProvider()>();
+
+  binder.bind<Pond>().to<Pond(Named<Animal, LieutenantShinysides>)>();
+}
+
 int main() {
+  sauce::shared_ptr<Injector> injector(Modules().add(&AnimalModule).createInjector());
+
   #ifdef __STRICT_ANSI__
   return 0;
   #else
