674795c3d3791b6fbb30bb8adc291651bb7c630a
Phil Smith <phil.h.smith@gmail.com>
2012-07-07 14:59:26 -0700

    Here's a tutorial idea.
    An MVC web app for placing tracking pizza orders.

--- test/tutorial_test.cc
+++ test/tutorial_test.cc
@@ -10,13 +10,26 @@ namespace test {
  * of a typical MVC-like application.  Unlike binding_test, whose aim is coverage across all variations of binding and
  * use, the focus here is on clarity and documentation.
  *
- * Now I just need an example app idea..
+ * Suppose we are creating a web application allowing a Mom & Pop pizza parlour to accept orders online.  Customers can
+ * choose toppings, sauce (ha!) and crust, specify take-out or delivery (giving an address as needed) and later inspect
+ * the progress of their order.  An employee at the store uses a similar interface to declare when an order has
+ * started, left for delivery, etc.  This application runs in a three-tier architecture of a web server backed by app
+ * processes talking to a single relational database.
+ *
+ * Even in this simple application there is complexity to manage, which can be addressed by following the MVC pattern.
+ * Vanilla MVC articulates a useful separation of concerns and wraps each in an object.  However it is silent about how
+ * those objects are ultimately assembled into the final application; this is where dependency injection (and sauce)
+ * can help.
+ *
+ * Since the point of the technique is to keep components decoupled and unaware of each other, a real application would
+ * likely spread its components out across many headers and compilation units.  This poses no problem for sauce, but
+ * for clarity everything in this example is contained in this suite.  Comments are included where suggested file
+ * breaks might occur.
  */
 
 TEST(TutorialTest, shouldExist) {
   ASSERT_TRUE(true);
 }
-
 
 }
 }
