67e327b91ff307afd91c0a34f16a2b15f240342d
Phil Smith <phil.h.smith@gmail.com>
2012-07-07 14:02:39 -0700

    Have the start of a working tutorial.
    ..at the request of a prospective user, even.  Squee!

---
+++ test/tutorial_test.cc
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include <sauce/sauce.h>
+
+namespace sauce {
+namespace test {
+
+/**
+ * This suite is intended to be a complete (if stubbed) example of how sauce might be used to manage the dependencies
+ * of a typical MVC-like application.  Unlike binding_test, whose aim is coverage across all variations of binding and
+ * use, the focus here is on clarity and documentation.
+ *
+ * Now I just need an example app idea..
+ */
+
+TEST(TutorialTest, shouldExist) {
+  ASSERT_TRUE(true);
+}
+
+
+}
+}
