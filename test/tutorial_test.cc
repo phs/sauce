057fbc04a709669f1f0cca24cf64e64029c2e1fe
Phil Smith <phil.h.smith@gmail.com>
2012-07-07 20:42:01 -0700

    More tutorial.

--- test/tutorial_test.cc
+++ test/tutorial_test.cc
@@ -2,7 +2,7 @@
 
 #include <sauce/sauce.h>
 
-namespace sauce {
+namespace sauce_tutorial { // Put in a different namespace, forcing us to show off "using" statements.
 namespace test {
 
 /**
@@ -37,7 +37,7 @@ namespace test {
  */
 
 // ********************************************************************************************************************
-// routes.h
+// web.h
 
 /**
  * This too is a library used by the application author, now to declare how incoming requests are mapped to the
@@ -49,13 +49,50 @@ namespace test {
 
 /**
  * This library just exposes the local clock, allowing the application author to sample timestamps.
+ *
+ * Mocking a clock for testing is a typical chore for temporal code; we will demonstrate doing this with sauce below.
  */
+
+// Seconds since the epoch.
+typedef int Timestamp;
+
+/**
+ * The clock interface.
+ *
+ * Typically this (and an adapter for the system clock) is supplied by the application or framework author.
+ */
+struct Clock {
+  virtual Timestamp timestamp() = 0;
+};
+
+/**
+ * "Real" clock implementation.
+ *
+ * Returned timestamps are taken from a counter that is bumped on each call to suggest an actual clock.
+ */
+struct SystemClock: public Clock {
+  Timestamp currentTime;
+
+  SystemClock():
+    currentTime(0) {}
+
+  Timestamp timestamp() {
+    return currentTime++;
+  }
+};
 
 // ********************************************************************************************************************
 // order_model.h
 
 /**
  * A pizza order being processed.
+ */
+
+// ********************************************************************************************************************
+// routes.h
+
+/**
+ * Mappings between url patterns and controllers, supplied by the application author, in our ficticuous framework.
  */
 
 // ********************************************************************************************************************
@@ -79,20 +116,83 @@ namespace test {
  * The unit test suite for PlaceController, demonstrating how to inject mocks or stubs.
  */
 
+using sauce::Binder;
+
+struct StubClock: public Clock {
+  Timestamp timestamp() {
+    return 0;
+  }
+};
+
+/**
+ * The sauce module, written by the application author, that specifies bindings for this unit test suite.
+ *
+ * Modules are collections of bindings, which tell sauce how interface requests should be satisfied.  The application
+ * author tells sauce about bindings with a "fluent" API exposed either by the passed Binder or inherited from
+ * AbstractModule (see below.)
+ *
+ * Each binding begins with a call to bind, passing the interface to be bound as a template parameter.  Further chained
+ * calls declare what concrete implementation should be used, and customize how to make or find it.
+ */
+void MockModule(Binder & binder) {
+  /**
+   * The result of bind() (an intermediate with an annoyingly complex type) is invoked in turn.
+   *
+   * Here, to<StubClock()>() says that requests for the interface Clock should be satisfied with instances of the
+   * StubClock concrete type.
+   *
+   * StubClock() is actually a function type.  This is how we specify which constructor to use.  If the chosen
+   * constructor takes arguments, they are treated as dependencies and satisfied first.
+   */
+  binder.bind<Clock>().to<StubClock()>();
+}
+
 // ********************************************************************************************************************
 // production_module.cc
 
+using sauce::AbstractModule;
+
 /**
  * The sauce module, written by the application author, that specifies the bindings used when running in production.
+ *
+ * Concretely, a module is either a void (Binder &) function as above, or a class deriving from AbstractModule as is
+ * the case here. (Technically any type providing a void operator()(Binder & binder) will do.)
+ *
+ * The difference is just sugar: the AbstractModule doesn't need to prefix bindings with "binder." while the function
+ * is conceptually simpler and more concrete.
  */
+class ProductionModule: public AbstractModule {
+  void configure() {
+    // Here there is no Binder, we just call bind<Iface>() directly.
+    bind<Clock>().to<SystemClock()>();
+  }
+};
 
 // ********************************************************************************************************************
 // main.cc
+
+using sauce::Modules;
+using sauce::Injector;
 
 /**
  * The entry point of the application, and where sauce injects dependencies.
  */
 TEST(TutorialTest, main) { // Let's pretend this is main()
+  /**
+   * Create an injector by first choosing what modules to use.  These are accumulated in a Modules object.
+   * More than one module can be selected, allowing the application developer to mix and match.
+   */
+  Modules modules;
+  modules.add(ProductionModule());
+
+  /**
+   * After desired modules are added, use createInjector() to get an injector itself.  It is returned as a
+   * sauce::shared_ptr, which is just an alias for std::shared_ptr (though std::tr1 and boost smart pointers can be
+   * used, if the SAUCE_STD_TR1_SMART_PTR or SAUCE_BOOST_SMART_PTR preprocessor symbols are defined.)
+   *
+   */
+  sauce::shared_ptr<Injector> injector = modules.createInjector();
+
   ASSERT_TRUE(true);
 }
 
