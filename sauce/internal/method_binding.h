12763e7ae3d7b7f194acea843f999d13033ac018
Phil Smith <phil.h.smith@gmail.com>
2012-07-15 20:27:31 -0700

    Flush out MethodBinding.
    There is a fair bit of common form with NewBinding.  Once the test passes I'll probable extract a VariadicBindingMixin or similar.

--- sauce/internal/method_binding.h
+++ sauce/internal/method_binding.h
@@ -3,6 +3,7 @@
 
 #include <sauce/injector.h>
 #include <sauce/scopes.h>
+#include <sauce/internal/apply_variadic.h>
 #include <sauce/internal/binding.h>
 #include <sauce/internal/resolved_binding.h>
 
@@ -11,7 +12,77 @@ namespace internal {
 
 template<typename Dependency, typename Method>
 class MethodBinding: public Binding<Dependency, NoScope> {
+
+  typedef MethodBinding<Dependency, Method> MethodBinding_;
+
+  /**
+   * A mixin for ApplyVariadic parameter concept types.
+   */
+  struct MethodBindingFriend {
+    template<typename T>
+    void validateAcyclicHelper(MethodBinding_ const & binding, InjectorPtr injector, TypeIds & ids, std::string name) {
+      binding.template validateAcyclicHelper<T>(injector, ids, name);
+    }
+
+    template<typename T>
+    typename Key<T>::Ptr getHelper(MethodBinding_ const & binding, InjectorPtr injector, std::string name) {
+      return binding.template getHelper<typename i::Key<T>::Normalized>(injector, name);
+    }
+  };
+
+  friend class MethodBindingFriend;
+
+  struct InjectParameters {
+    struct Passed {
+      MethodBinding_ const & binding;
+      InjectorPtr & injector;
+
+      Passed(MethodBinding_ const & binding, InjectorPtr & injector):
+        binding(binding), injector(injector) {}
+    };
+
+    template<typename T, int i, typename Passed>
+    struct Parameter: public MethodBindingFriend {
+      typedef typename Key<T>::Ptr Ptr;
+
+      Ptr yield(Passed passed) {
+        MethodBinding_ const & binding = passed.binding;
+        InjectorPtr & injector = passed.injector;
+        std::string dependencyName = binding.dynamicDependencyNames[i];
+
+        return this->template getHelper<T>(binding, injector, dependencyName);
+      }
+    };
+  };
+
+  typedef ApplyMethod<InjectParameters, Method> Inject;
+  typedef typename Key<Dependency>::Iface Iface;
   typedef typename Key<Dependency>::Ptr IfacePtr;
+
+  std::vector<std::string> dynamicDependencyNames;
+
+  struct ValidateAcyclicParameters {
+    struct Passed {
+      MethodBinding_ const & binding;
+      InjectorPtr & injector;
+      TypeIds & ids;
+
+      Passed(MethodBinding_ const & binding, InjectorPtr & injector, TypeIds & ids):
+        binding(binding), injector(injector), ids(ids) {}
+    };
+
+    template<typename T, int i, typename Passed>
+    struct Parameter: public MethodBindingFriend {
+      void observe(Passed passed) {
+        MethodBinding_ const & binding = passed.binding;
+        InjectorPtr & injector = passed.injector;
+        TypeIds & ids = passed.ids;
+        std::string dependencyName = binding.dynamicDependencyNames[i];
+
+        this->template validateAcyclicHelper<T>(binding, injector, ids, dependencyName);
+      }
+    };
+  };
 
   Method method;
 
@@ -19,16 +90,24 @@ class MethodBinding: public Binding<Dependency, NoScope> {
     return true;
   }
 
+  void validateAcyclic(InjectorPtr injector, TypeIds & ids) const {
+    typename ValidateAcyclicParameters::Passed passed(*this, injector, ids);
+    // observeMethod<ValidateAcyclicParameters>(method, passed);
+  }
+
 public:
 
   typedef typename ResolvedBinding<Dependency>::BindingPtr BindingPtr;
 
   explicit MethodBinding(Method method):
+    dynamicDependencyNames(Inject::arity(), unnamed()),
     method(method) {}
 
-  void inject(IfacePtr &, BindingPtr, InjectorPtr) const {}
+  void inject(IfacePtr & /* injected */, BindingPtr, InjectorPtr injector) const {
+    typename InjectParameters::Passed passed(*this, injector);
+    // applyMethod<InjectParameters>(*injected.get(), method, passed);
+  }
 
-  void validateAcyclic(InjectorPtr, TypeIds &) const {}
 };
 
 }
