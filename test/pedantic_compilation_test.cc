6a28ae72cc10f4de9682b2b78d5b3372ac7f30fe
Phil Smith <phil.h.smith@gmail.com>
2015-08-27 23:45:33 -0700

    Ditch `ansi_compilation_test` for per-standard tests.
    First, `cxx98_pedantic_compilation_test` is always present and runs with `-std=c++98` and
    `-pedantic-errors`.  `-ansi` is dropped since it just seems to alias a particular `-std` in
    gcc and clang.
    
    After bringing in a stock c++11 detection autoconf macro, use it to determine availability
    of C++11.  If present, use it to compile `cxx11_pedantic_compilation_test`.

--- test/pedantic_compilation_test.cc
+++ test/pedantic_compilation_test.cc
@@ -3,10 +3,10 @@
 #include <sauce/sauce.h>
 
 /**
- * The purpose of this test is to enforce ansi c++ compatibility within the source.
+ * The purpose of this test is to enforce c++ standards compatibility within the source.
  *
  * It does so by first pulling in all desired includes, instantiating templates and then
- * asserting that it was compiled with -ansi.
+ * asserting that it was compiled with __STRICT_ANSI__.
  */
 
 using ::sauce::AbstractProvider;
@@ -80,7 +80,7 @@ int main() {
   #ifdef __STRICT_ANSI__
   return 0;
   #else
-  std::cerr << "This file was not compiled with -ansi." << std::endl;
+  std::cerr << "This file was not compiled with a strict standard." << std::endl;
   return 1;
   #endif
 }
