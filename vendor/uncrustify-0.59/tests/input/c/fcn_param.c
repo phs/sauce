f58105f84f752964eb221721642cfbcd812ce6b9
Phil Smith <phil.h.smith@gmail.com>
2015-08-29 01:42:19 -0700

    Ditch uncrustify, it was always crap.
    Ditch the coverage check as well, since it only works with GCC.

--- vendor/uncrustify-0.59/tests/input/c/fcn_param.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void x(custom_t *e, void (*funcptr));
-void x(custom_t *e, void (*funcptr)[]);
-void x(custom_t *e, void (*funcptr)(int, int));
-void x(custom_t *e, void (*funcptr)(int, int)[]);
-
